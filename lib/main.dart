c60834f095ce905e95e7372d11f0a6dfe2c507de
Rafitos <rafaelsevla@gmail.com>
2025-09-18 12:06:16 -0300

    improve styles

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,6 @@ class MyHomePage extends StatelessWidget {
 
   MyHomePage({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,11 +57,51 @@ class MyHomePage extends StatelessWidget {
               child: Text('Gráfico'),
             ),
           ),
-          Column(children: _transactions.map((tr) {
-            return Card(
-              child: Text(tr.title)
-            );
-          }).toList()),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple[50]!, width: 2),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        'R\$ ${tr.value.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tr.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          tr.date.toString(),
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
         ],
       ),
     );
